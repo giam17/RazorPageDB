de3999437312ffe8955160eb6ba284277e594272
Giam <tejerinalucass4@gmail.com>
2025-08-26 21:01:29 -0400

    ClaseDB

---
+++ WARazorDB/WARazorDB/Data/TareaDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WARazorDB.Models;
+
+namespace WARazorDB.Data
+{
+    public class TareaDBContext : DbContext
+    {
+        public TareaDBContext(DbContextOptions <TareaDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarea> Tareas { get; set; }
+
+        protected TareaDBContext()
+        {
+        }
+    }
+}
